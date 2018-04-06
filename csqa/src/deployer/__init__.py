c17b283b6f524d5ed404a6f3cffda8fcf0b00ed7
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-06 09:57:44 +0200

    Create the project structure

---
+++ csqa/src/deployer/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""csqa is a package for training a dialogue system for the Complex Sequential Question Answering (CSQA) task.
+
+
+Installation
+------------
+Get the Latest
+~~~~~~~~~~~~~~~
+Download the most recent code from ... with:
+
+
+For Developers
+~~~~~~~~~~~~~~
+Clone the repository from `GitHub <>'
+
+"""
+
+
+__version__ = '0.1.1-dev'
+
+__title__ = 'sda_csqa'
+__description__ = "A package for creating a dialogue system for the Complex Sequential Question Answering (CSQA) task"
+__url__ = ''
+
+__author__ = 'Mehdi Ali'
+__email__ = 'alim@informatik.uni-bonn.de'
+
