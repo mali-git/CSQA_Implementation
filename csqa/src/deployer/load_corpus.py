2bd9a5b39ee70c7c82e483de8d0a296c99c15f0c
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-06 10:34:49 +0200

    Fix the imports and apply minor changes

--- csqa/src/deployer/load_corpus.py
+++ csqa/src/deployer/load_corpus.py
@@ -1,10 +1,10 @@
-
-import click
 import logging
-from utilities.corpus_preprocessing_utilities.load_dialogues import get_files_from_direc, load_dialogues_from_json_file
+import click
+from utilities.corpus_preprocessing.load_dialogues import get_files_from_direc
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
+
 
 @click.command()
 @click.option('-input', help='path to corpus directory', required=True)
@@ -12,14 +12,6 @@ def main(input):
     input_direc = input
     log.info("Load files from %s" % (input_direc))
     files = get_files_from_direc(input_direc)
-
-    for file in files:
-        instances = load_dialogues_from_json_file(file)
-        print(instances)
-        for instance in instances:
-            print(instance)
-        exit(0)
-
 
 
 if __name__ == '__main__':
