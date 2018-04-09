d69d7d63c40fcf57dd5730ec533ea76b03852047
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-09 14:59:17 +0200

    Create unique script for retrieving dialogues from disk

--- csqa/src/utilities/corpus_preprocessing/load_dialogues.py
+++ csqa/src/utilities/corpus_preprocessing/load_dialogues.py
@@ -1,10 +1,43 @@
 import json
 import logging
+import timeit
 from pathlib import Path
 import os
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
+
+
+def retrieve_dialogues(input_directory):
+    """
+    Retreieve dialogues from disk and save in a dict.
+    :param input_directory: Path to the root directory
+    :rtype: dict
+    """
+    files = get_files_from_direc(input_directory)
+    dialogues_data_dict = _create_dialogue_data_dict(files=files)
+    return dialogues_data_dict
+
+def _create_dialogue_data_dict(files):
+    log.info("Extract dialogues from files")
+    start = timeit.default_timer()
+    dialogues_dict = dict()
+    for file in files:
+        log.debug("Process file %s" % file)
+        try:
+            dialogue_in_file = load_dialogues_from_json_file(file)
+        except:
+            log.info("Problem with file %s " % (file))
+            continue
+
+        parts_of_path = Path(file).parts
+        key = os.path.join(parts_of_path[-3], parts_of_path[-2], parts_of_path[-1])
+        dialogues_dict[key] = dialogue_in_file
+        log.debug("File %s loaded." % (file))
+
+    stop = timeit.default_timer()
+    log.info("Dialogues extracted. It took %s seconds \n" % (str(round(stop - start))))
+    return dialogues_dict
 
 
 def load_dialogues_from_json_file(input_path):
@@ -25,7 +58,8 @@ def get_files_from_direc(path_to_directory):
     :param path_to_directory: Path to the root directory
     :rtype: list
     """
-
+    log.info("Load files from %s" % (path_to_directory))
+    start = timeit.default_timer()
     if Path(path_to_directory).name.startswith('.'):
         raise Exception("Root directory shouldn't be a hidden directory.")
 
@@ -47,4 +81,6 @@ def get_files_from_direc(path_to_directory):
     else:
         raise Exception("%s is not a directory" % (path_to_directory))
 
+    stop = timeit.default_timer()
+    log.info("Files loaded. It took %s seconds \n" % (str(round(stop - start))))
     return files
