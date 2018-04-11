154053fbb03142295ea72d76f910acffca65ae38
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-11 16:30:39 +0200

    Correct errors and add word_vector specification

--- csqa/src/utilities/tensor_embeddings_creation/feature_utils.py
+++ csqa/src/utilities/tensor_embeddings_creation/feature_utils.py
@@ -1,8 +1,8 @@
 from collections import OrderedDict
-from utilities.constants import POSITION_VEC_DIM, PART_OF_SPEECH_VEC_DIM
+from utilities.constants import POSITION_VEC_DIM, PART_OF_SPEECH_VEC_DIM, WORD_VEC_DIM
 
 
-def get_feature_specification_dict(position_vec_dim=None, part_of_speech_vec_dim=None):
+def get_feature_specification_dict(word_vec_dim=None, position_vec_dim=None, part_of_speech_vec_dim=None):
     """
     Get dictionaty containing the information about additional features. If the dimension of a feature is specified,
     then this feature will be used.
@@ -12,12 +12,16 @@ def get_feature_specification_dict(position_vec_dim=None, part_of_speech_vec_dim
     """
     feature_spec_dict = OrderedDict()
 
+    if word_vec_dim is not None:
+        assert (type(word_vec_dim),int)
+        feature_spec_dict[WORD_VEC_DIM] = word_vec_dim
+
     if position_vec_dim is not None:
-        assert (position_vec_dim,type(int))
+        assert (type(position_vec_dim),int)
         feature_spec_dict[POSITION_VEC_DIM] = position_vec_dim
 
     if part_of_speech_vec_dim is not None:
-        assert (part_of_speech_vec_dim,type(int))
+        assert (type(part_of_speech_vec_dim), int)
         feature_spec_dict[PART_OF_SPEECH_VEC_DIM] = part_of_speech_vec_dim
 
     return feature_spec_dict
