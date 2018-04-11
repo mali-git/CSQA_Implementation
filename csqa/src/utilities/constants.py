69962ba96ef39381c57be2d7a97b3c642fcce420
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-11 15:16:10 +0200

    Add constants

--- csqa/src/utilities/constants.py
+++ csqa/src/utilities/constants.py
@@ -7,3 +7,21 @@ QUESTION_UTTERANCE = 'question_utterance'
 ANSWER_UTTERANCE = 'answer_utterance'
 QUESTION_UTTERANCE_EMBEDDED = 'question_utterance_embedded'
 ANSWER_UTTERANCE_EMBEDDED = 'answer_utterance_embedded'
+
+# Given keys in the training corpus
+CSQA_ALL_ENTITIES = 'all_entities'
+CSQA_SPEAKER = 'speaker'
+CSQA_ENTITIES_IN_UTTERANCE = 'entities_in_utterance'
+CSQA_UTTERANCE = 'utterance'
+CSQA_ACTIVE_SET = 'active_set'
+CSQA_DESCRIPTION = 'description'
+CSQA_QUES_TYPE_ID = 'ques_type_id'
+CSQA_QUESTION_TYPE = 'question-type'
+CSQA_RELATIONS = 'relations'
+CSQA_TYPE_LIST = 'type_list'
+
+# Features (position features, part-of-speech features etc.) related
+POSITION_VEC_DIM = 'position_vec_dim'
+PART_OF_SPEECH_VEC_DIM = 'part_of_speech_vec_dim'
+
+
