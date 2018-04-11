007ffcfc7c2061162598cb5df08f2b62d2eb93a2
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-11 10:26:27 +0200

    Add constants needed for representation of the instances

---
+++ csqa/src/utilities/constants.py
@@ -0,0 +1,9 @@
+
+# Instance related
+INSTANCE_ID = 'instance_id'
+ENTITY_ID = 'entity_id'
+PREDICATE_ID = 'predicate_id'
+QUESTION_UTTERANCE = 'question_utterance'
+ANSWER_UTTERANCE = 'answer_utterance'
+QUESTION_UTTERANCE_EMBEDDED = 'question_utterance_embedded'
+ANSWER_UTTERANCE_EMBEDDED = 'answer_utterance_embedded'
