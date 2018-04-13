98e00758212bf3ed02b1de863f4c0e17875b7203
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-14 01:37:40 +0200

    Replace save_insertion_of_offsets() with more efficient version

--- csqa/src/utilities/corpus_preprocessing/text_manipulation_utils.py
+++ csqa/src/utilities/corpus_preprocessing/text_manipulation_utils.py
@@ -7,54 +7,44 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def save_insertion_of_offsets(start_offsets, end_offsets, start, end):
-    # Get the smallest value where start can be inserted in the sorted list
-    index_of_smallest = bisect.bisect(start_offsets, start)
-    indices_of_overlapped_offsets = []
-    save_start_offsets = []
-    save_end_offsets = []
-
-    if index_of_smallest == 0:
-        log.info(
-            "start is not in start offsets --> All existing end offsets smaller than current end are overlapped")
-        # start is not in start offsets --> All existing end offsets smaller than current end are overlapped
-        temp_np_array = np.array(end_offsets, dtype=int)
-        current_indices_of_none_overlapped_offsets = ((temp_np_array > end) * 1).nonzero()
-        save_start_offsets = start_offsets[current_indices_of_none_overlapped_offsets]
-        save_end_offsets = end_offsets[current_indices_of_none_overlapped_offsets]
-
-
-    elif index_of_smallest == len(start_offsets):
-        # start is greater then all current values --> there are no overlapping offsets
-        save_start_offsets = start_offsets
-        save_end_offsets = end_offsets
-
+def handle_invalid_case(start, end, problematic_start, problematic_end):
+    if (end - start) > (problematic_end - problematic_start):
+        return start, end
     else:
-        # start lies between smallest and biggest current start
+        return problematic_start, problematic_end
 
-        # Step 1: Get indices of end positions which are bigger or equal to current end
-        temp_np_array = np.array(end_offsets, dtype=int)
-        current_end_offsets_indices_bigger_or_equal = ((temp_np_array >= end) * 1).nonzero()
-        non_over_lapping_offsets = np.ones(shape=len(start_offsets)).tolist()
-        insertion_allowed = False
 
-        # Step 2: Check relevant start indices
-        # relevant_start_indices = start_offsets[current_end_offsets_indices_bigger_or_equal]
-        for relevant_index in current_end_offsets_indices_bigger_or_equal:
-            current_start = start_offsets[relevant_index]
-            if current_start <= start:
-                # Case: ['Chancellor of Germany'] Insert: 'Germany' -> Don't insert
-                continue
-            else:
-                # Case: ['Germany'] Insert: 'Chancellor of Germany' -> Insert
-                non_over_lapping_offsets[relevant_index] = 0
-                insertion_allowed = True
+def save_insertion_of_offsets(start_offsets, end_offsets, start, end):
+    start_offsets = np.array(start_offsets, dtype=int)
+    end_offsets = np.array(end_offsets, dtype=int)
+    indices = np.ones(shape=len(start_offsets))
 
-        # Step 3: Determine safe start offsets
-        save_start_offsets = np.array(start_offsets[non_over_lapping_offsets], dtype=int).nonzero().tolist()
-        # Insert new start if possible
-        if insertion_allowed:
-            bisect.insort_left(save_start_offsets, start)
-            bisect.insort_left(save_end_offsets, end)
+    # Case 1: New entity is contained in existing entity
+    case_one_invalid = (((start_offsets <= start) and (end_offsets >= start)) * 1).nonzero()
+    # Case 2: New entity contains other entities
+    case_two_invalid = (((start_offsets >= start) and (end_offsets <= end)) * 1).nonzero()
 
-    return save_start_offsets, save_end_offsets
+    mask = case_one_invalid if (len(case_one_invalid[0]) == 1) else case_two_invalid
+
+    problematic_start, problematic_end = start_offsets[mask][0], end_offsets[mask][0]
+    valid_start, valid_end = handle_invalid_case(start=start, end=end, problematic_start=problematic_start,
+                                                 problematic_end=problematic_end)
+
+    start_offsets = start_offsets.tolist()
+    end_offsets = end_offsets.tolist()
+    index = mask[0][0]
+
+    if valid_start==start_offsets[index] and valid_end==end_offsets[index]:
+        log.info("Don't insert new entity since it is covered by exsting entity")
+    else:
+        log.info("Conflict due to overlapping of entities")
+        # Remove invalid offsets and add valid ones
+        log.info("Remove start offsets (start:%s,end:%s)" % (start,end))
+        del start_offsets[index]
+        del end_offsets[index]
+        bisect.insort_left(start_offsets, valid_start)
+        bisect.insort_left(end_offsets, valid_end)
+
+    return start_offsets, end_offsets
+
+
