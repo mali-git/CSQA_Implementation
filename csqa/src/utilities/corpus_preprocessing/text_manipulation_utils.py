d9bbd612718eb36ea054695bbc3da936ea41dcb8
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-14 01:52:40 +0200

    Add function documentations and apply minor chnages

--- csqa/src/utilities/corpus_preprocessing/text_manipulation_utils.py
+++ csqa/src/utilities/corpus_preprocessing/text_manipulation_utils.py
@@ -7,28 +7,44 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def handle_invalid_case(start, end, problematic_start, problematic_end):
-    if (end - start) > (problematic_end - problematic_start):
-        return start, end
+def get_offsets_of_entity_with_longer_span(new_start, new_end, existing_start, existing_end):
+    """
+    Returns offsets of entity with bigger span
+    :param new_start: Start position of new entity
+    :param new_end: End position of new entity
+    :param existing_start: Start position of existing entity
+    :param existing_end: End position of existing entity
+    :rtype: int,int
+    """
+    if (new_end - new_start) > (existing_end - existing_start):
+        return new_start, new_end
     else:
-        return problematic_start, problematic_end
+        return existing_start, existing_end
 
 
-def save_insertion_of_offsets(start_offsets, end_offsets, start, end):
+def save_insertion_of_offsets(start_offsets, end_offsets, new_start, new_end):
+    """
+    Will safely add new offsets i.e. overlapping cases of entities will be solved.
+    :param start_offsets: Start positions of current entities
+    :param end_offsets: End positions of current entities
+    :param new_start: Start position of new entity
+    :param new_end: End position of new entity
+    :rtype: list,list
+    """
     start_offsets = np.array(start_offsets, dtype=int)
     end_offsets = np.array(end_offsets, dtype=int)
     indices = np.ones(shape=len(start_offsets))
 
-    # Case 1: New entity is contained in existing entity
-    case_one_invalid = (((start_offsets <= start) and (end_offsets >= start)) * 1).nonzero()
-    # Case 2: New entity contains other entities
-    case_two_invalid = (((start_offsets >= start) and (end_offsets <= end)) * 1).nonzero()
+    # Case 1: New entity is contained in existing entity. ['Chancellor of Germany'] Insert: 'Germany'
+    case_one_overlappings = (((start_offsets <= new_start) and (end_offsets >= new_start)) * 1).nonzero()
+    # Case 2: New entity contains other entities. ['Germany'] Insert: 'Chancellor of Germany'
+    case_two_overlappings = (((start_offsets >= new_start) and (end_offsets <= new_end)) * 1).nonzero()
 
-    mask = case_one_invalid if (len(case_one_invalid[0]) == 1) else case_two_invalid
+    mask = case_one_overlappings if (len(case_one_overlappings[0]) == 1) else case_two_overlappings
 
     problematic_start, problematic_end = start_offsets[mask][0], end_offsets[mask][0]
-    valid_start, valid_end = handle_invalid_case(start=start, end=end, problematic_start=problematic_start,
-                                                 problematic_end=problematic_end)
+    valid_start, valid_end = get_offsets_of_entity_with_longer_span(new_start=new_start, new_end=new_end, existing_start=problematic_start,
+                                                                    existing_end=problematic_end)
 
     start_offsets = start_offsets.tolist()
     end_offsets = end_offsets.tolist()
@@ -39,7 +55,7 @@ def save_insertion_of_offsets(start_offsets, end_offsets, start, end):
     else:
         log.info("Conflict due to overlapping of entities")
         # Remove invalid offsets and add valid ones
-        log.info("Remove start offsets (start:%s,end:%s)" % (start,end))
+        log.info("Remove start offsets (start:%s,end:%s)" % (new_start, new_end))
         del start_offsets[index]
         del end_offsets[index]
         bisect.insort_left(start_offsets, valid_start)
