c7cec344499b4bbbbc4b47b2bcbf9eaf28019817
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-14 03:06:13 +0200

    Implement further tests for save_insertion_of_offsets()

--- csqa/tests/test_utterance_to_tensor_embeddings_creator.py
+++ csqa/tests/test_utterance_to_tensor_embeddings_creator.py
@@ -1,34 +1,65 @@
-
-import unittest
 import logging
+import unittest
 
 from utilities.corpus_preprocessing.text_manipulation_utils import save_insertion_of_offsets
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
-class TestUtterance2TensorCreator(unittest.TestCase):
 
+class TestUtterance2TensorCreator(unittest.TestCase):
     def test_save_insertion_of_offsets(self):
         start_offsets = [10]
         end_offsets = [40]
 
         # Case: ['Chancellor of Germany'] Insert: 'Germany' --> Don't insert
-        start = 30
-        end = 40
+        start_new = 30
+        end_new = 40
 
-        updated_start_offsets, updated_end_offsets = save_insertion_of_offsets(start_offsets, end_offsets, start, end)
+        updated_start_offsets, updated_end_offsets = save_insertion_of_offsets(start_offsets, end_offsets, start_new,
+                                                                               end_new)
         # print(updated_start_offsets)
 
-        self.assertTrue(len(updated_start_offsets)==len(updated_end_offsets)==1)
-        self.assertEqual(updated_start_offsets[0],start_offsets[0])
-        self.assertEqual(updated_end_offsets[0],end_offsets[0])
+        self.assertTrue(len(updated_start_offsets) == len(updated_end_offsets) == 1)
+        self.assertEqual(updated_start_offsets[0], start_offsets[0])
+        self.assertEqual(updated_end_offsets[0], end_offsets[0])
 
         # Case: ['Germany'] Insert: 'Chancellor of Germany' --> Insert
-        start = 5
-        end = 40
+        start_new = 5
+        end_new = 40
 
-        updated_start_offsets, updated_end_offsets = save_insertion_of_offsets(start_offsets, end_offsets, start, end)
-        print(updated_start_offsets)
+        updated_start_offsets, updated_end_offsets = save_insertion_of_offsets(start_offsets, end_offsets, start_new,
+                                                                               end_new)
         self.assertTrue(len(updated_start_offsets) == len(updated_end_offsets) == 1)
+        self.assertEqual(updated_start_offsets[0], start_new)
+        self.assertEqual(updated_end_offsets[0], end_new)
+
+        # Case: Add non-overlapping entity
+        start_new = 5
+        end_new = 20
+
+        updated_start_offsets, updated_end_offsets = save_insertion_of_offsets(start_offsets, end_offsets, start_new,
+                                                                               end_new)
+
+        self.assertTrue(len(updated_start_offsets) == len(updated_end_offsets) == 2)
+        self.assertEqual(updated_start_offsets[0], start_new)
+        self.assertEqual(updated_end_offsets[0], end_new)
+        self.assertEqual(updated_start_offsets[1], start_offsets[0])
+        self.assertEqual(updated_end_offsets[1], end_offsets[0])
+
+        # Case: Entity overlaps with more than one entity -> Don't insert new entity
+        start_offsets = [10, 25]
+        end_offsets = [20, 35]
+        start_new = 15
+        end_new = 25
+
+        updated_start_offsets, updated_end_offsets = save_insertion_of_offsets(start_offsets, end_offsets, start_new,
+                                                                               end_new)
+
+        self.assertTrue(len(updated_start_offsets) == len(updated_end_offsets) == 2)
+        self.assertEqual(updated_start_offsets[0], start_offsets[0])
+        self.assertEqual(updated_end_offsets[0], end_offsets[0])
+        self.assertEqual(updated_start_offsets[1], start_offsets[1])
+        self.assertEqual(updated_end_offsets[1], end_offsets[1])
+
 
