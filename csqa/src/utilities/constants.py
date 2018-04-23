4c5b6684ff768fa25ee2fd34d7005b48a9744cf9
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-04-23 10:01:12 +0200

    Add new constants needed by deployer scripts

--- csqa/src/utilities/constants.py
+++ csqa/src/utilities/constants.py
@@ -1,14 +1,14 @@
 
 # Instance related
 INSTANCE_ID = 'instance_id'
-QUESTION_ENTITIES = 'entities_in_question'
-ANSWER_ENTITIES = 'entities_in_answer'
 ENTITY_ID = 'entity_id'
 PREDICATE_IDS_QUESTION = 'predicate_ids_question'
 PREDICATE_IDS_ANSWER = 'predicate_ids_answer'
+QUESTION_ENTITIES = 'entities_in_question'
 QUESTION_UTTERANCE = 'question_utterance'
-ANSWER_UTTERANCE = 'answer_utterance'
 QUESTION_UTTERANCE_EMBEDDED = 'question_utterance_embedded'
+ANSWER_ENTITIES = 'entities_in_answer'
+ANSWER_UTTERANCE = 'answer_utterance'
 ANSWER_UTTERANCE_EMBEDDED = 'answer_utterance_embedded'
 
 # Given keys in the training corpus
@@ -27,5 +27,9 @@ CSQA_TYPE_LIST = 'type_list'
 WORD_VEC_DIM = 'word_vec_dim'
 POSITION_VEC_DIM = 'position_vec_dim'
 PART_OF_SPEECH_VEC_DIM = 'part_of_speech_vec_dim'
+
+# Needed for configuration
+PATH_TO_WORD_TO_WEC_MODELS = 'path_to_word_vec_models'
+PATH_TO_ENTITY_MAPPING_FILE = 'path_to_entity_mapping_file'
 
 
