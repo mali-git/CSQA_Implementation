026470487947df09052b6d9e1ca42424d1e16749
Mehdi Ali <ali-mehdi@live.de>
2018-05-17 16:43:05 +0200

    Adapt code, so that batching of dialogues instead of batching of utterances is supported

--- csqa/src/neural_network_models/csqa_network.py
+++ csqa/src/neural_network_models/csqa_network.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 from collections import OrderedDict
 
 import tensorflow as tf
@@ -24,88 +25,93 @@ class CSQANetwork(object):
         :param params: Dictionary containing model parameters
         :return:
         """
-        # Important: The utterances in a batch represent a specific dialogue context. Therefore, utterances
-        # in batch are related to each other.
-        # Shape: (batch_size, max_utter_length, vec_dimension)
-        embedded_sequences = features[EMBEDDED_SEQUENCES]
-        embedded_responses = features[EMBEDDED_RESPONSES]
-        sequenece_lengths = self._compute_sequence_lengths(embedded_sequences)
-        sequenece_lengths_responses = self._compute_sequence_lengths(embedded_responses)
-        self.key_cells = features[KEY_CELLS]
-        self.value_cells = features[VALUE_CELLS]
 
-        # ----------------Hierarchical Encoder----------------
-        with tf.variable_scope('Utterance Level Encoder'):
-            utterance_level_encoder = tf.nn.rnn_cell.LSTMCell(num_units=params[NUM_UNITS_HRE_UTTERANCE_CELL])
+        dialgoue_representations = []
 
-            # state_tuple is a tuple containing the last hidden state and the last activation
-            _, state_tuple_utter_level = tf.nn.dynamic_rnn(
-                cell=utterance_level_encoder,
-                dtype=tf.float32,
-                sequence_length=sequenece_lengths,
-                inputs=embedded_sequences)
+        for feature in features:
+            # Shape: (num_utterances, max_utter_length, vec_dimension)
+            embedded_sequences = feature[EMBEDDED_SEQUENCES]
+            embedded_response = feature[EMBEDDED_RESPONSES]
+            sequenece_lengths = self._compute_sequence_lengths(embedded_sequences)
+            sequenece_lengths_responses = self._compute_sequence_lengths(embedded_response)
+            key_cells = features[KEY_CELLS]
+            value_cells = features[VALUE_CELLS]
 
-            # For each sequence extract the last hidden state
-            # Shape of last [batch_size, NUM_UNITS_IN_LSTM_CELL]
-            utterances_last_hidden_states = state_tuple_utter_level[0]
-            shape_last_hidden_states = tf.shape(utterances_last_hidden_states)
-            num_context_utterances = shape_last_hidden_states[0]
-            dimension_hidden_utterance_state = shape_last_hidden_states[1]
-            # Reshape [batch_size, NUM_UNITS_IN_LSTM_CELL] to [batch_size,
-            # 1, NUM_UNITS_IN_LSTM_CELL]
-            # since each hidden state represents a summary with NUM_UNITS_IN_LSTM_CELL features
-            utterances_last_hidden_states = tf.reshape(utterances_last_hidden_states, shape=(
-                num_context_utterances, 1, dimension_hidden_utterance_state))
+            # ----------------Hierarchical Encoder----------------
+            with tf.variable_scope('Utterance Level Encoder'):
+                utterance_level_encoder = tf.nn.rnn_cell.LSTMCell(num_units=params[NUM_UNITS_HRE_UTTERANCE_CELL])
 
-        with tf.variable_scope('Context Level Encoder'):
-            context_level_encoder = tf.nn.rnn_cell.LSTMCell(num_units=params[NUM_UNITS_HRE_CONTEXT_CELL])
+                # state_tuple is a tuple containing the last hidden state and the last activation
+                _, state_tuple_utter_level = tf.nn.dynamic_rnn(
+                    cell=utterance_level_encoder,
+                    dtype=tf.float32,
+                    sequence_length=sequenece_lengths,
+                    inputs=embedded_sequences)
 
-            _, state_tuple_context_level = tf.nn.dynamic_rnn(
-                cell=context_level_encoder,
-                dtype=tf.float32,
-                # Shape of each last hidden state is (1, NUM_UNITS_HRED_UTTERANCE_CELL)
-                sequence_length=[1 for _ in range(num_context_utterances)],
-                inputs=utterances_last_hidden_states)
+                # For each sequence extract the last hidden state
+                # Shape of last [num_utterances, NUM_UNITS_IN_LSTM_CELL]
+                utterances_last_hidden_states = state_tuple_utter_level[0]
+                shape_last_hidden_states = tf.shape(utterances_last_hidden_states)
+                num_context_utterances = shape_last_hidden_states[0]
+                dimension_hidden_utterance_state = shape_last_hidden_states[1]
+                # Reshape [num_utterances, NUM_UNITS_IN_LSTM_CELL] to [num_utterances,
+                # 1, NUM_UNITS_IN_LSTM_CELL]
+                # since each hidden state represents a summary with NUM_UNITS_IN_LSTM_CELL features
+                utterances_last_hidden_states = tf.reshape(utterances_last_hidden_states, shape=(
+                    num_context_utterances, 1, dimension_hidden_utterance_state))
 
-            context_last_hidden_states = state_tuple_context_level[0]
-            shape_context_last_hidden_states = tf.shape(context_last_hidden_states)
-            # Reshape [batch_size, NUM_UNITS_IN_CONTEXT_CELL] to [batch_size, 1, NUM_UNITS_IN_CONTEXT_CELL]
-            # context_last_hidden_states = tf.reshape(context_last_hidden_states, shape=(
-            #     shape_context_last_hidden_states[0], 1, shape_context_last_hidden_states[1]))
+            with tf.variable_scope('Context Level Encoder'):
+                context_level_encoder = tf.nn.rnn_cell.LSTMCell(num_units=params[NUM_UNITS_HRE_CONTEXT_CELL])
+
+                _, state_tuple_context_level = tf.nn.dynamic_rnn(
+                    cell=context_level_encoder,
+                    dtype=tf.float32,
+                    # Shape of each last hidden state is (1, NUM_UNITS_HRED_UTTERANCE_CELL)
+                    sequence_length=[1 for _ in range(num_context_utterances)],
+                    inputs=utterances_last_hidden_states)
+
+                # Only hidden state of last input to context encoder is relevant
+                # since it represents the last dialogue state
+                lastest_dialogue_representation = state_tuple_context_level[0][-1]
+                dialgoue_representations.append(lastest_dialogue_representation)
+
+
+        #[batch_size, NUM_UNITS_HRED_UTTERANCE_CELL]
+        dialgoue_representations = np.array(dialgoue_representations,dtype=np.float32)
 
         # ----------------Key-Value Memory Network----------------
         with tf.variable_scope('Key Value Memory Network'):
             word_vec_dim = params[WORD_VEC_DIM]
             feature_size = params[NUM_UNITS_HRE_CONTEXT_CELL]
             num_hops = params[NUM_HOPS]
-            initial_queries = context_last_hidden_states
+            initial_queries = dialgoue_representations
 
             # R_i: Matrix used to get query representation q_i+1
             self.R = [tf.Variable(
                 tf.truncated_normal([feature_size, feature_size],
                                     stddev=0.1)) for R_i in range(num_hops)]
             self.A = tf.Variable(
-                tf.truncated_normal([feature_size, feature_size], stddev=0.1),
+                tf.truncated_normal([feature_size, word_vec_dim], stddev=0.1),
                 name="A")
 
             # output after last iteration over memory adressing/reading
             # Shape: (batch_size, feature_size)
-            output_queries = self._get_response_from_memory(num_hops=num_hops, initial_queries=initial_queries)
+            memory_output = self._get_response_from_memory(num_hops=num_hops, initial_queries=initial_queries)
 
         # ----------------Decoder----------------
         train_loss = None
 
         with tf.variable_scope('Decoder'):
-            batch_size, _ = tf.shape(output_queries)
+            batch_size, _ = tf.shape(memory_output)
             decoder_cell = tf.nn.rnn_cell.LSTMCell(num_units=params[WORD_VEC_DIM])
 
-            helper = self.get_helper(mode, decoder_embedded_input=embedded_responses,
+            helper = self.get_helper(mode, decoder_embedded_input=embedded_response,
                                      sequenece_lengths=sequenece_lengths_responses, params=params)
 
             projection_layer = tf.layers.dense(
                 units=params[VOCABUALRY_SIZE], use_bias=False)
 
-            decoder = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cell, helper=helper, initial_state=output_queries,
+            decoder = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cell, helper=helper, initial_state=memory_output,
                                                       output_layer=projection_layer)
 
             outputs, final_context_state, final_sequence_lengths = tf.contrib.seq2seq.dynamic_decode(decoder=decoder)
@@ -163,7 +169,7 @@ class CSQANetwork(object):
             return get_estimator_specification(mode=mode, predictions_dict=predictions_dict,
                                                classifier_output=classification_output, loss=train_loss)
 
-    def _get_response_from_memory(self, num_hops, initial_queries):
+    def _get_response_from_memory(self, num_hops, initial_queries, key_cells, value_cells):
         """
 
         :param num_hops:
@@ -182,11 +188,12 @@ class CSQANetwork(object):
         for hop in range(num_hops):
             log.info("----------Key Adressing----------")
             # shape = [k,m,n] transpose to [m,k,n]
-            self.key_cells = tf.transpose(self.key_cells, [m, k, n])
-            self.key_cells = tf.reshape(self.key_cells, shape=(m, k * n))
+            key_cells = tf.transpose(key_cells, [1, 0, 2])
+            # [embedding_size, batch_size * memory_size]
+            key_cells = tf.reshape(key_cells, shape=(m, k * n))
 
             # Shape: (feature_size, batch_size * memory_size)
-            A_keys = tf.matmul(self.A, self.key_cells)
+            A_keys = tf.matmul(self.A, key_cells)
             # Reshape to (batch_size, memory_size, feature_size)
             A_keys = tf.transpose(A_keys)
             A_keys = tf.reshape(A_keys, shape=(-1, n, m))
@@ -203,10 +210,10 @@ class CSQANetwork(object):
 
             log.info("----------Value Reading----------")
             # For each batch element the columns represent the values
-            self.value_cells = tf.transpose(self.value_cells, [m, k, n])
-            self.value_cells = tf.reshape(self.value_cells, shape=(m, k * n))
+            value_cells = tf.transpose(value_cells, [1, 0, 2])
+            value_cells = tf.reshape(value_cells, shape=(m, k * n))
             # Shape: (feature_size, batch_size * memory_size)
-            A_values = tf.matmul(self.A, self.value_cells)
+            A_values = tf.matmul(self.A, value_cells)
             # Reshape to (batch_size, memory_size, feature_size)
             A_values = tf.transpose(A_values)
             A_values = tf.reshape(A_values, shape=(-1, n, m))
