c8e22b0a572db962aefe7ae753c2d617c63de573
Mehdi Ali <ali-mehdi@live.de>
2018-05-24 14:06:55 +0200

    Debug network

--- csqa/src/deployer/csqa_exmpl_deployer.py
+++ csqa/src/deployer/csqa_exmpl_deployer.py
@@ -1,22 +1,27 @@
-from collections import OrderedDict
 import logging
+import os
+import time
+from collections import OrderedDict
+
 import click
 import numpy as np
+import tensorflow as tf
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 from neural_network_models.csqa_network import CSQANetwork
-from utilities.constants import EMBEDDED_SEQUENCES, EMBEDDED_RESPONSES, NUM_UNITS_HRE_UTTERANCE_CELL, \
+from utilities.constants import NUM_UNITS_HRE_UTTERANCE_CELL, \
     NUM_UNITS_HRE_CONTEXT_CELL, NUM_HOPS, WORD_VEC_DIM, VOCABUALRY_SIZE, LEARNING_RATE, OPTIMIZER, ADAM, \
-    MAX_NUM_UTTER_TOKENS
+    MAX_NUM_UTTER_TOKENS, BATCH_SIZE, NUM_TRAINABLE_TOKENS, RESPONSES
 
 
 @click.command()
 @click.option('-vocab_path', help='path to vocabulary file', required=True)
 @click.option('-keys_path', help='path to file containing embedded keys', required=True)
 @click.option('-values_path', help='path to file containing embedded values', required=True)
-def main(vocab_path, keys_path, values_path):
+@click.option('-output_direc', help='path to output directory', required=True)
+def main(vocab_path, keys_path, values_path, output_direc):
     utter_1 = 'We are building a new dialogue system'
     utter_2 = 'What is a dialogue system?'
     keys = ['rel_1_subj_1', 'rel_1_subj_2', 'rel_1_subj_3']
@@ -34,30 +39,23 @@ def main(vocab_path, keys_path, values_path):
     utter_1_ids = add_padding(sequence=utter_1_ids, max_length=max_length, voc_to_id=voc_to_id)
     utter_2_ids = add_padding(sequence=utter_2_ids, max_length=max_length, voc_to_id=voc_to_id)
 
-    batch_keys_embeddings = np.array([key_embeddings.T], dtype=np.float32)
-    batch_value_embeddings = np.array([value_embeddings.T], dtype=np.float32)
+    keys_embeddings = np.array([key_embeddings.T], dtype=np.float32)
+    value_embeddings = np.array([value_embeddings.T], dtype=np.float32)
 
-    instance_1 = OrderedDict()
-    instance_1[EMBEDDED_SEQUENCES] = np.array([utter_1_ids, utter_2_ids])
-    instance_1[EMBEDDED_RESPONSES] = np.array([utter_2])
+    dialogues = np.array([[utter_1_ids, utter_2_ids]], dtype=np.int32)
 
-    features = OrderedDict()
-    features['BATCH_DIALOGUES'] = [instance_1]
-    features['BATCH_KEYS'] = batch_keys_embeddings
-    features['BATCH VALUES'] = batch_value_embeddings
+    log.info('-------DIALOGUES-------')
+    log.info(dialogues.shape)
 
-    log.info('-------BATCH_DIALOGUES-------')
-    log.info(len(features['BATCH_DIALOGUES']))
+    log.info('-------KEYS-------')
+    log.info(keys_embeddings.shape)
 
-    log.info('-------BATCH_KEYS-------')
-    log.info(features['BATCH_KEYS'].shape)
-
-    log.info('-------BATCH VALUES-------')
-    log.info(features['BATCH VALUES'].shape)
+    log.info('-------VALUES-------')
+    log.info(value_embeddings.shape)
 
     # Define model parameters
     model_params = OrderedDict()
-    model_params[NUM_UNITS_HRE_UTTERANCE_CELL] = 10
+    model_params[NUM_UNITS_HRE_UTTERANCE_CELL] = 15
     model_params[NUM_UNITS_HRE_CONTEXT_CELL] = 5
     model_params[NUM_HOPS] = 2
     model_params[WORD_VEC_DIM] = 2
@@ -65,13 +63,27 @@ def main(vocab_path, keys_path, values_path):
     model_params[LEARNING_RATE] = 0.001
     model_params[OPTIMIZER] = ADAM
     model_params[MAX_NUM_UTTER_TOKENS] = max_length
+    model_params[NUM_TRAINABLE_TOKENS] = num_trainable_tokens
+    model_params[BATCH_SIZE] = 1
+
+    currentTime = time.strftime("%H:%M:%S")
+    currentDate = time.strftime("%d/%m/%Y").replace('/', '-')
+    output_direc += '/' + currentDate + '_' + currentTime + ''
+    os.makedirs(output_direc)
 
     # TODO: Adapt stategy for passing arguments
-    model = CSQANetwork(num_trainable_tokens=num_trainable_tokens, word_vec_dim=2, vocab_size=17,
-                initial_embeddings=voc_embeddings)
+    model = CSQANetwork(initial_embeddings=voc_embeddings)
 
-    
+    # configuration = tf.contrib.learn.RunConfig(save_checkpoints_secs=3000, gpu_memory_fraction=0.9
 
+    nn = tf.estimator.Estimator(model_fn=model.model_fct, params=model_params,
+                                model_dir=output_direc,
+                                config=None)
+
+    nn.train(input_fn=lambda: input_fct(dialogues=dialogues, responses=np.array([utter_2_ids]),
+                                        keys_embedded=keys_embeddings, values_embedded=value_embeddings,
+                                        batch_size=1),
+             steps=100)
 
 
 def process_embedding_file(path_to_file):
@@ -99,6 +111,25 @@ def add_padding(sequence, max_length, voc_to_id):
     padding = [voc_to_id['<pad>'] for i in range(padding_size)]
 
     return sequence + padding
+
+
+def input_fct(dialogues, responses, keys_embedded, values_embedded, batch_size):
+    dialogues, responses, keys_embedded, values_embedded = tf.train.slice_input_producer(
+        [dialogues, responses, keys_embedded, values_embedded],
+        shuffle=False)
+
+    dataset_dict = dict(dialogues=dialogues, responses=responses, keys_embedded=keys_embedded,
+                        values_embedded=values_embedded)
+
+    batch_dicts = tf.train.batch(dataset_dict, batch_size=batch_size,
+                                 num_threads=1, capacity=batch_size * 2,
+                                 enqueue_many=False, shapes=None, dynamic_pad=False,
+                                 allow_smaller_final_batch=False,
+                                 shared_name=None, name=None)
+
+    responses = batch_dicts.pop(RESPONSES)
+
+    return batch_dicts, responses
 
 
 if __name__ == '__main__':
--- csqa/src/neural_network_models/csqa_network.py
+++ csqa/src/neural_network_models/csqa_network.py
@@ -1,14 +1,15 @@
 import logging
 from collections import OrderedDict
-
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.estimator.export import export_output
+from tensorflow.python.layers import core as layers_core
 
-from utilities.constants import EMBEDDED_SEQUENCES, NUM_UNITS_HRE_UTTERANCE_CELL, NUM_UNITS_HRE_CONTEXT_CELL, NUM_HOPS, \
-    WORD_VEC_DIM, KEY_CELLS, VALUE_CELLS, VOCABUALRY_SIZE, EMBEDDED_RESPONSES, LEARNING_RATE, OPTIMIZER, LOGITS, \
-    WORD_PROBABILITIES, WORD_IDS, TARGET_SOS_ID, TARGET_EOS_ID, MAX_NUM_UTTER_TOKENS
-from utilities.tensorflow_estimator_utils import get_estimator_specification
+from utilities.constants import NUM_UNITS_HRE_UTTERANCE_CELL, NUM_UNITS_HRE_CONTEXT_CELL, NUM_HOPS, \
+    WORD_VEC_DIM, VOCABUALRY_SIZE, LEARNING_RATE, OPTIMIZER, LOGITS, \
+    WORD_PROBABILITIES, WORD_IDS, TARGET_SOS_ID, TARGET_EOS_ID, MAX_NUM_UTTER_TOKENS, BATCH_SIZE, NUM_TRAINABLE_TOKENS, \
+    DIALOGUES
+from utilities.tensorflow_estimator_utils import get_estimator_specification, get_optimizer
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -16,11 +17,9 @@ log = logging.getLogger(__name__)
 
 class CSQANetwork(object):
 
-    def __init__(self, num_trainable_tokens, word_vec_dim, vocab_size, initial_embeddings=None):
+    def __init__(self, initial_embeddings=None):
         self.initial_embeddings = initial_embeddings
-        self.embeddings = self.initialize_embedding_layer(embeddings=self.initial_embeddings,
-                                                          num_trainable_tokens=num_trainable_tokens,
-                                                          word_vec_dim=word_vec_dim, vocab_size=vocab_size)
+        self.embeddings = None
 
     def initialize_embedding_layer(self, embeddings, num_trainable_tokens, word_vec_dim, vocab_size):
         with tf.variable_scope('embedding_layer'):
@@ -32,9 +31,9 @@ class CSQANetwork(object):
                 initializer = tf.constant_initializer(embeddings)
 
                 trainable_embeddings = tf.get_variable(name="trainable_embeddings",
-                                                 shape=[num_trainable_tokens, word_vec_dim],
-                                                 initializer=tf.constant_initializer(trainable_embeddings),
-                                                 trainable=True)
+                                                       shape=[num_trainable_tokens, word_vec_dim],
+                                                       initializer=tf.constant_initializer(trainable_embeddings),
+                                                       trainable=True)
 
                 pretrained_embeddings = tf.get_variable(name="pretrained_embeddings",
                                                         shape=[vocab_size - num_trainable_tokens, word_vec_dim],
@@ -51,70 +50,83 @@ class CSQANetwork(object):
 
             return embeddings
 
-    def model_fct(self, features, targets, mode, params):
+    def model_fct(self, features, labels, mode, params):
         """
         This function defines the neural network called by tf.Estimator
         :param features: Features of instances
-        :param targets: Responses to last question in each batch
+        :param labels: Responses to last question in each batch
         :param mode: Defines in which mode function is called (train, eval or test)
         :param params: Dictionary containing model parameters
         :return:
         """
-
         dialgoue_representations = []
+        batch_dialogues = features[DIALOGUES]
 
-        for feature in features:
+        # TODO: Change until 30.05.2018
+        embedded_keys = features['keys_embedded']
+        embedded_values = features['values_embedded']
+
+        if self.embeddings is None:
+            self.embeddings = self.initialize_embedding_layer(embeddings=self.initial_embeddings,
+                                                              num_trainable_tokens=params[NUM_TRAINABLE_TOKENS],
+                                                              word_vec_dim=params[WORD_VEC_DIM],
+                                                              vocab_size=params[VOCABUALRY_SIZE])
+
+        for i in range(params[BATCH_SIZE]):
+            # Shape: [num_utterances, max_utter_length]
+            dialogue = batch_dialogues[i]
+
             # Shape: (num_utterances, max_utter_length, vec_dimension)
-            embedded_sequences = tf.nn.embedding_lookup(self.embeddings, feature[EMBEDDED_SEQUENCES])
-            embedded_response = tf.nn.embedding_lookup(self.embeddings, feature[EMBEDDED_RESPONSES])
+            embedded_dialogue = tf.nn.embedding_lookup(self.embeddings, dialogue)
+            embedded_response = tf.nn.embedding_lookup(self.embeddings, labels[i])
+            embedded_response = tf.expand_dims(input=embedded_response, axis=0)
 
-            exit(0)
-
-            sequenece_lengths = self._compute_sequence_lengths(embedded_sequences)
+            sequenece_lengths = self._compute_sequence_lengths(embedded_dialogue)
             sequenece_lengths_responses = self._compute_sequence_lengths(embedded_response)
-            key_cells = features[KEY_CELLS]
-            value_cells = features[VALUE_CELLS]
+            key_cells = embedded_keys
+            value_cells = embedded_values
 
             # ----------------Hierarchical Encoder----------------
             with tf.variable_scope('utterance_level_encoder'):
                 utterance_level_encoder = tf.nn.rnn_cell.LSTMCell(num_units=params[NUM_UNITS_HRE_UTTERANCE_CELL])
 
                 # state_tuple is a tuple containing the last hidden state and the last activation
-                _, state_tuple_utter_level = tf.nn.dynamic_rnn(
+                _, lstm_state_tuple_utter_level = tf.nn.dynamic_rnn(
                     cell=utterance_level_encoder,
                     dtype=tf.float32,
                     sequence_length=sequenece_lengths,
-                    inputs=embedded_sequences)
+                    inputs=embedded_dialogue)
 
                 # For each sequence extract the last hidden state
                 # Shape of last [num_utterances, NUM_UNITS_IN_LSTM_CELL]
-                utterances_last_hidden_states = state_tuple_utter_level[0]
+                utterances_last_hidden_states = lstm_state_tuple_utter_level[0]
                 shape_last_hidden_states = tf.shape(utterances_last_hidden_states)
                 num_context_utterances = shape_last_hidden_states[0]
                 dimension_hidden_utterance_state = shape_last_hidden_states[1]
+
                 # Reshape [num_utterances, NUM_UNITS_IN_LSTM_CELL] to [num_utterances,
                 # 1, NUM_UNITS_IN_LSTM_CELL]
                 # since each hidden state represents a summary with NUM_UNITS_IN_LSTM_CELL features
                 utterances_last_hidden_states = tf.reshape(utterances_last_hidden_states, shape=(
-                    num_context_utterances, 1, dimension_hidden_utterance_state))
+                    num_context_utterances, 1, params[NUM_UNITS_HRE_UTTERANCE_CELL]))
 
             with tf.variable_scope('context_level_encoder'):
                 context_level_encoder = tf.nn.rnn_cell.LSTMCell(num_units=params[NUM_UNITS_HRE_CONTEXT_CELL])
 
-                _, state_tuple_context_level = tf.nn.dynamic_rnn(
+                _, lstm_cell_tuple_context_level = tf.nn.dynamic_rnn(
                     cell=context_level_encoder,
                     dtype=tf.float32,
                     # Shape of each last hidden state is (1, NUM_UNITS_HRED_UTTERANCE_CELL)
-                    sequence_length=[1 for _ in range(num_context_utterances)],
+                    sequence_length=tf.ones(num_context_utterances),
                     inputs=utterances_last_hidden_states)
 
                 # Only hidden state of last input to context encoder is relevant
                 # since it represents the last dialogue state
-                lastest_dialogue_representation = state_tuple_context_level[0][-1]
+                lastest_dialogue_representation = lstm_cell_tuple_context_level[0][-1]
                 dialgoue_representations.append(lastest_dialogue_representation)
 
         # [batch_size, NUM_UNITS_HRED_UTTERANCE_CELL]
-        dialgoue_representations = np.array(dialgoue_representations, dtype=np.float32)
+        dialgoue_representations = tf.stack(dialgoue_representations)
 
         # ----------------Key-Value Memory Network----------------
         with tf.variable_scope('key_value_memory_network'):
@@ -140,35 +152,44 @@ class CSQANetwork(object):
         train_loss = None
 
         with tf.variable_scope('decoder'):
-            batch_size, _ = tf.shape(memory_output)
-            decoder_cell = tf.nn.rnn_cell.LSTMCell(num_units=params[WORD_VEC_DIM])
+            batch_size = tf.shape(memory_output)[0]
+            decoder_cell = tf.nn.rnn_cell.LSTMCell(num_units=feature_size)
+
+            decoder_seq_lengths = np.array(np.repeat(a=params[MAX_NUM_UTTER_TOKENS], repeats=params[BATCH_SIZE]),
+                                           dtype=np.int32)
 
             helper = self.get_helper(mode, decoder_embedded_input=embedded_response,
-                                     sequenece_lengths=sequenece_lengths_responses, params=params)
+                                     sequenece_lengths=decoder_seq_lengths, params=params)
 
-            projection_layer = tf.layers.dense(
-                units=params[VOCABUALRY_SIZE], use_bias=False)
+            projection_layer = layers_core.Dense(units=params[VOCABUALRY_SIZE], use_bias=False)
 
-            decoder = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cell, helper=helper, initial_state=memory_output,
+            initial_state_tuple = tf.contrib.rnn.LSTMStateTuple(c=memory_output,
+                                                                h=tf.zeros(shape=(batch_size, feature_size)))
+
+            decoder = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cell, helper=helper,
+                                                      initial_state=initial_state_tuple,
                                                       output_layer=projection_layer)
 
             outputs, final_context_state, final_sequence_lengths = tf.contrib.seq2seq.dynamic_decode(
                 decoder=decoder)
+
             logits = outputs.rnn_output
+
             predicted_word_ids = outputs.sample_id
 
             if mode != tf.estimator.ModeKeys.PREDICT:
                 # target's shape: [batch_size, max_seq_length] logit's shape: [batch_size, max_seq_length, word_vec_dim]
                 cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                    labels=targets, logits=logits)
+                    labels=labels, logits=logits)
 
                 # Mask padding vectors
                 # target_weights shape: [batch_size, max_seq_len]
                 target_weights = tf.sequence_mask(lengths=sequenece_lengths_responses,
                                                   maxlen=params[MAX_NUM_UTTER_TOKENS],
                                                   dtype=logits.dtype)
+
                 # Normalize loss based on batch_size
-                train_loss = (tf.reduce_sum(cross_entropy * target_weights) / batch_size)
+                train_loss = (tf.reduce_sum(cross_entropy * target_weights) / tf.to_float(batch_size))
 
         # ----------------Prepare Output----------------
 
@@ -191,9 +212,9 @@ class CSQANetwork(object):
         # If no learning rate is specified then use the default value in the case specified optimizer
         #  has a default value
         if LEARNING_RATE in params:
-            optimizer = self.get_optimizer(optimizer=params[OPTIMIZER], learning_rate=params[LEARNING_RATE])
+            optimizer = get_optimizer(optimizer=params[OPTIMIZER], learning_rate=params[LEARNING_RATE])
         else:
-            optimizer = self.get_optimizer(optimizer=params[OPTIMIZER])
+            optimizer = get_optimizer(optimizer=params[OPTIMIZER])
 
         train_op = optimizer.minimize(train_loss, global_step=tf.train.get_global_step())
 
@@ -221,41 +242,58 @@ class CSQANetwork(object):
         # Shape: (batch_size, feature_size)
         queries = initial_queries
         shape_qs = tf.shape(queries)
+        batch_size = shape_qs[0]
+        feature_size = shape_qs[1]
+
         # For each batch element the columns represent the keys
-        k, m, n = tf.shape(self.key_cells)
+        shape_key_cells = tf.shape(key_cells)
+        k_keys = shape_key_cells[0]
+        m_keys = shape_key_cells[1]
+        n_keys = shape_key_cells[2]
+
+        shape_value_cells = tf.shape(value_cells)
+        k_values = shape_value_cells[0]
+        m_values = shape_value_cells[1]
+        n_values = shape_value_cells[2]
+
+        # shape = [k_keys,m_keys,n_keys] transpose to [m_keys,k_keys,n_keys]
+        key_cells = tf.transpose(key_cells, [1, 0, 2])
+        # [embedding_size, batch_size * memory_size]
+        key_cells = tf.reshape(key_cells, shape=(m_keys, k_keys * n_keys))
+
+        # For each batch element the columns represent the values
+        value_cells = tf.transpose(value_cells, [1, 0, 2])
+        value_cells = tf.reshape(value_cells, shape=(m_values, k_values * n_values))
 
         for hop in range(num_hops):
             log.info("----------Key Adressing----------")
-            # shape = [k,m,n] transpose to [m,k,n]
-            key_cells = tf.transpose(key_cells, [1, 0, 2])
-            # [embedding_size, batch_size * memory_size]
-            key_cells = tf.reshape(key_cells, shape=(m, k * n))
-
             # Shape: (feature_size, batch_size * memory_size)
             A_keys = tf.matmul(self.A, key_cells)
+
             # Reshape to (batch_size, memory_size, feature_size)
             A_keys = tf.transpose(A_keys)
-            A_keys = tf.reshape(A_keys, shape=(-1, n, m))
+            A_keys = tf.reshape(A_keys, shape=(-1, n_keys, feature_size))
 
             # Reshape to (batch_size, 1, feature_size)
-            queries_reshaped = tf.reshape(queries, shape=(shape_qs[0], 1, shape_qs[1]))
+            queries_reshaped = tf.reshape(queries, shape=(batch_size, 1, feature_size))
+
             # Computed dot product between A_queries and A_keys
             # Multiply element wise and then add along columns: Compute dot product for each element of batch
             memory_confidence_scores = tf.reduce_sum(queries_reshaped * A_keys, 2)
+
             # Shape: (batch_size, memory_size)
             memory_probabilities = tf.nn.softmax(memory_confidence_scores)
+
             # Expand to (batch_size, memory_size, 1)
             memory_probabilities = tf.expand_dims(memory_probabilities, axis=-1)
 
             log.info("----------Value Reading----------")
-            # For each batch element the columns represent the values
-            value_cells = tf.transpose(value_cells, [1, 0, 2])
-            value_cells = tf.reshape(value_cells, shape=(m, k * n))
+
             # Shape: (feature_size, batch_size * memory_size)
             A_values = tf.matmul(self.A, value_cells)
             # Reshape to (batch_size, memory_size, feature_size)
             A_values = tf.transpose(A_values)
-            A_values = tf.reshape(A_values, shape=(-1, n, m))
+            A_values = tf.reshape(A_values, shape=(-1, n_values, feature_size))
 
             # [batch_size, feature_size]
             o = tf.reduce_sum(memory_probabilities * A_values, 1)
@@ -265,6 +303,7 @@ class CSQANetwork(object):
             R = self.R[hop]
             # Shape: (feature_size, batch_size)
             queries = tf.matmul(a=R, b=o, transpose_b=True)
+
             # Transpose to (batch_size, feature_size)
             queries = tf.transpose(queries)
 
